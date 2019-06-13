1f0cef2bba5fa95163311f537a459bc1016b9ccd
Pedro Rohr <pedro.rohr@warren.wrn>
2019-06-13 16:10:02 -0300

    feat: add simple controller and configure some hangfire jobs

--- Hangfire-PoC/Controllers/HomeController.cs
+++ Hangfire-PoC/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hangfire_PoC.Controllers
@@ -10,6 +11,11 @@ namespace Hangfire_PoC.Controllers
     {
         public IActionResult Index()
         {
+            BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget Job Executed"));
+            BackgroundJob.Schedule(() => Console.WriteLine("Delayed job executed"), TimeSpan.FromMinutes(1));
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Minutely Job executed"), Cron.Minutely);
+            var id = BackgroundJob.Enqueue(() => Console.WriteLine("Hello, "));
+            BackgroundJob.ContinueJobWith(id, () => Console.WriteLine("world!"));
             return View();
         }
     }
